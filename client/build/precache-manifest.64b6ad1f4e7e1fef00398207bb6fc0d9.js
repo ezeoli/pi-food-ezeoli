543528d8dc975773f5f80493a5e28e9c9130e080
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-02-24 04:29:59 -0300

    depley

---
+++ client/build/precache-manifest.64b6ad1f4e7e1fef00398207bb6fc0d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ef7d903acaf54f44d0",
+    "url": "/static/css/main.005d633a.chunk.css"
+  },
+  {
+    "revision": "59ef7d903acaf54f44d0",
+    "url": "/static/js/main.b58ceb3b.chunk.js"
+  },
+  {
+    "revision": "8b27081b5e6c84271250",
+    "url": "/static/js/runtime~main.8a079fb4.js"
+  },
+  {
+    "revision": "bc1376aa4c6157d98519",
+    "url": "/static/js/2.cad165f5.chunk.js"
+  },
+  {
+    "revision": "5a96c0dbfc76f561e7f2",
+    "url": "/static/js/3.5d1c5066.chunk.js"
+  },
+  {
+    "revision": "9426c29df6b70b6e94745c9520233009",
+    "url": "/static/media/comida.9426c29d.jpg"
+  },
+  {
+    "revision": "ee1d081c5bdf966b058c1a6588e73e8a",
+    "url": "/static/media/loading.ee1d081c.gif"
+  },
+  {
+    "revision": "b6548f483ebca5398efdec84e05be00d",
+    "url": "/index.html"
+  }
+];
