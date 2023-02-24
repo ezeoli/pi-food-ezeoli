44ca34c5230927264fda0cb18f25229985426493
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-02-24 06:19:12 -0300

    dee

--- api/index.js
+++ api/index.js
@@ -20,10 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {loadData} = require('./src/data/loadData.js');
+require('dotenv').config();
+const {PORT} = process.env
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   await loadData();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ', PORT); // eslint-disable-line no-console
   });
 });
