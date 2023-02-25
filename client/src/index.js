447f000164ce1a57f774e17af4c3ac60f8552c84
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-02-25 03:41:33 -0300

    axios

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import store from './redux/store';
 import axios from 'axios';
 
 //axios.defaults.baseURL='http://localhost:3001';
+
 axios.defaults.baseURL='https://pi-food-ezeoli-production.up.railway.app';
 
 
