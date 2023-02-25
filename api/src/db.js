7845c82e9cb038378434ab3844c5c2fc1a05ffaf
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-02-25 02:42:10 -0300

    SS

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
@@ -10,6 +10,7 @@ const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+// eslint-disable-next-line no-undef
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
