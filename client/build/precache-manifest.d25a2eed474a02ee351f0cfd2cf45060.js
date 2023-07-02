a1ff1a1643416561705485ea32601e4954585143
ezeee <ezeolier@hotmail.com>
2023-07-02 05:15:15 -0300

    production july done

---
+++ client/build/precache-manifest.d25a2eed474a02ee351f0cfd2cf45060.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac914e9a5f0e42c73bee",
+    "url": "/static/css/main.66e674f0.chunk.css"
+  },
+  {
+    "revision": "ac914e9a5f0e42c73bee",
+    "url": "/static/js/main.1bf3ae82.chunk.js"
+  },
+  {
+    "revision": "bc8bb01d43bd4f629d21",
+    "url": "/static/js/runtime~main.9a7f8d4f.js"
+  },
+  {
+    "revision": "e7fbcc554364251e6bc9",
+    "url": "/static/js/2.0e340afb.chunk.js"
+  },
+  {
+    "revision": "1998d2b56f1a3017ca83",
+    "url": "/static/js/3.5a9d5fa9.chunk.js"
+  },
+  {
+    "revision": "9426c29df6b70b6e94745c9520233009",
+    "url": "/static/media/comida.9426c29d.jpg"
+  },
+  {
+    "revision": "ee1d081c5bdf966b058c1a6588e73e8a",
+    "url": "/static/media/loading.ee1d081c.gif"
+  },
+  {
+    "revision": "4578f32390de20f7219320a12fb947e5",
+    "url": "/index.html"
+  }
+];
